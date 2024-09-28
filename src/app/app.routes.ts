b0ec9af536cb79d7a8b3627a311b53396d6585f5
AyushSama <aayush0029@gmail.com>
2024-09-28 15:23:19 +0530

    Api Setup Completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AnalyseComponent } from './components/analyse/analyse.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        redirectTo : 'analyse',
+        pathMatch : 'full'
+    },
+    {
+        path : 'analyse',
+        component : AnalyseComponent,
+    }
+
+];
